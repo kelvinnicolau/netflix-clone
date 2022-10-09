5526aee7e11f840984f4bc6f5d37675202cd5922
Kelvin <kelvinnicolau7@gmail.com>
2022-10-09 15:58:38 -0300

    Rolagem ok

--- src/components/MovieRow/Index.js
+++ src/components/MovieRow/Index.js
@@ -16,7 +16,7 @@ export default ({title, itens}) => {
 
     const handleRightArrow = () => {
         let x = scrollX - Math.round(window.innerWidth / 2);
-        let listW = itens.results.length + 150;
+        let listW = itens.results.length * 150;
         if((window.innerWidth - listW) > x){
             x = (window.innerWidth - listW) - 60;
         }
